e82862ce88cb0460c441ef3b0a7410d6a55864a1
daeun-dev <daeunjang.dev@gmail.com>
2021-04-24 23:41:57 +0900

    commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <ToolBar></ToolBar>
+    <main></main>
+    <router-view></router-view>
+    
+  </div>
+</template>
+
+<script>
+import ToolBar from './components/ToolBar.vue'
+export default {
+  components :{
+    ToolBar,
+  },
+}
+</script>
+
+<style>
+
+body{
+  padding :0;
+  margin :0;
+}
+</style>
---
+++ src/views/AskView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <div v-for="item in ask" v-bind:key="item.title">{{ item.title }}</div>
+      </div>
+</template>
+
+<script>
+import { fetchAskList } from '../api/index.js'
+export default {
+    data(){
+        return{
+            ask : []
+        }
+    },
+    created(){
+        var vm= this;
+        fetchAskList()
+            .then(function(response){
+                vm.ask =  response.data;
+            })
+            .catch(function(error){
+                console.log(error);
+            })   
+ }
+}
+</script>
+
+<style>
+
+</style>
